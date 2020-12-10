55eb0d9900fd7bb187320c294e0379379b376c56
Cyril Kato <contact@cyril.email>
2020-12-10 10:05:07 +0100

    chore(test): replace AVA by UVU

--- index.js
+++ index.js
@@ -1,66 +1,63 @@
-const FEEN = {
-  dump: function(positionObject) {
-    var squareStr = [];
+exports.dump = function (positionObject) {
+  var squareStr = [];
 
-    for (let i = 0; i < positionObject['shape'][0]; i++) {
-      let empty = 0;
+  for (let i = 0; i < positionObject['shape'][0]; i++) {
+    let empty = 0;
 
-      for (let j = 0; j < positionObject['shape'][1]; j++) {
-        var squareId = j + (i * positionObject['shape'][0]);
-        var piece = positionObject['square'][squareId];
+    for (let j = 0; j < positionObject['shape'][1]; j++) {
+      var squareId = j + (i * positionObject['shape'][0]);
+      var piece = positionObject['square'][squareId];
 
-        if (piece) {
-          if (empty > 0) {
-            squareStr.push(empty);
-            empty = 0;
-          }
-
-          squareStr.push(piece);
-        } else {
-          empty++;
+      if (piece) {
+        if (empty > 0) {
+          squareStr.push(empty);
+          empty = 0;
         }
-      }
 
-      if (empty > 0) {
-        squareStr.push(empty);
+        squareStr.push(piece);
+      } else {
+        empty++;
       }
-
-      squareStr.push('/');
     }
-    squareStr.pop();
-    squareStr = squareStr.join(',');
-    squareStr = squareStr.replace(/,?\/,?/g, '/');
 
-    return [
-      squareStr,
-      positionObject['sideId'],
-      (positionObject['inHand'].length === 0 ? '-' : positionObject['inHand'].sort().join(','))
-    ].join(' ');
-  },
-  parse: function(feenString) {
-    const fields = feenString.split(' ', 3);
+    if (empty > 0) {
+      squareStr.push(empty);
+    }
 
-    const squareStr = fields[0];
-    const sideIdStr = fields[1];
-    const inHandStr = fields[2];
-
-    const numberRegex = /\d+/;
-    const lengthFirstDim = squareStr.split('/')[0].split(',').map(x => (numberRegex.test(x) ? parseInt(x, 10) : 1)).reduce((a, b) => a + b);
-    const squares = squareStr.split('/').map(row => row.split(',').map(cell => (numberRegex.test(cell) ? new Array(parseInt(cell, 10)).fill(null) : cell))).flat(2);
-    const boardObj = {...squares};
-
-    // NOTE remove the keys having a `null` value from the board object.
-    Object.keys(boardObj).forEach((key) => (boardObj[key] == null) && delete boardObj[key]);
-
-    const positionObject = {
-      inHand: (inHandStr === '-' ? [] : inHandStr.split(',')).sort(),
-      shape: [lengthFirstDim, lengthFirstDim],
-      sideId: parseInt(sideIdStr, 10),
-      square: boardObj
-    };
-
-    return positionObject;
+    squareStr.push('/');
   }
-};
+  squareStr.pop();
+  squareStr = squareStr.join(',');
+  squareStr = squareStr.replace(/,?\/,?/g, '/');
 
-module.exports = FEEN;
+  return [
+    squareStr,
+    positionObject['sideId'],
+    (positionObject['inHand'].length === 0 ? '-' : positionObject['inHand'].sort().join(','))
+  ].join(' ');
+}
+
+exports.parse = function (feenString) {
+  const fields = feenString.split(' ', 3);
+
+  const squareStr = fields[0];
+  const sideIdStr = fields[1];
+  const inHandStr = fields[2];
+
+  const numberRegex = /\d+/;
+  const lengthFirstDim = squareStr.split('/')[0].split(',').map(x => (numberRegex.test(x) ? parseInt(x, 10) : 1)).reduce((a, b) => a + b);
+  const squares = squareStr.split('/').map(row => row.split(',').map(cell => (numberRegex.test(cell) ? new Array(parseInt(cell, 10)).fill(null) : cell))).flat(2);
+  const boardObj = {...squares};
+
+  // NOTE remove the keys having a `null` value from the board object.
+  Object.keys(boardObj).forEach((key) => (boardObj[key] == null) && delete boardObj[key]);
+
+  const positionObject = {
+    inHand: (inHandStr === '-' ? [] : inHandStr.split(',')).sort(),
+    shape: [lengthFirstDim, lengthFirstDim],
+    sideId: parseInt(sideIdStr, 10),
+    square: boardObj
+  };
+
+  return positionObject;
+}
--- test.js
+++ test.js
@@ -1,31 +1,32 @@
-const Test = require('ava');
+const { test } = require('uvu');
+const assert = require('uvu/assert');
 const FEEN = require('./index');
 
 // Dump a classic Tsume Shogi problem
 
-const feenString = FEEN.dump({
-  inHand: ['S', 'r', 'r', 'b', 'g', 'g', 'g', 'g', 's', 'n', 'n', 'n', 'n', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-  shape: [9, 9],
-  sideId: 0,
-  square: {
-     3: 's',
-     4: 'k',
-     5: 's',
-    22: '+P',
-    43: '+B'
-  }
-});
+test('the dumped position', () => {
+  const feenString = FEEN.dump({
+    inHand: ['S', 'r', 'r', 'b', 'g', 'g', 'g', 'g', 's', 'n', 'n', 'n', 'n', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+    shape: [9, 9],
+    sideId: 0,
+    square: {
+       3: 's',
+       4: 'k',
+       5: 's',
+      22: '+P',
+      43: '+B'
+    }
+  });
 
-Test('the dumped position', t => {
-  t.is(feenString, '3,s,k,s,3/9/4,+P,4/9/7,+B,1/9/9/9/9 0 S,b,g,g,g,g,n,n,n,n,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,r,r,s');
+  assert.is(feenString, '3,s,k,s,3/9/4,+P,4/9/7,+B,1/9/9/9/9 0 S,b,g,g,g,g,n,n,n,n,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,r,r,s');
 });
 
 // Parse a classic Tsume Shogi problem
 
-const positionObject = FEEN.parse('3,s,k,s,3/9/4,+P,4/9/7,+B,1/9/9/9/9 0 S,b,g,g,g,g,n,n,n,n,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,r,r,s');
+test('the parsed position', () => {
+  const positionObject = FEEN.parse('3,s,k,s,3/9/4,+P,4/9/7,+B,1/9/9/9/9 0 S,b,g,g,g,g,n,n,n,n,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,p,r,r,s');
 
-Test('the parsed position', t => {
-  t.deepEqual(positionObject, {
+  assert.equal(positionObject, {
     inHand: ['S', 'b', 'g', 'g', 'g', 'g', 'n', 'n', 'n', 'n', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'r', 'r', 's'],
     shape: [9, 9],
     sideId: 0,
@@ -38,3 +39,5 @@ Test('the parsed position', t => {
     }
   });
 });
+
+test.run();
